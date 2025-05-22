e9c1b5c8cbc639e5eb273c5ddaae9a08d3151b09
Daniel Nasonov <dan.nov345@gmail.com>
2025-05-22 20:58:23 +0200

    Refactor

--- drum_kit_btns/script.js
+++ drum_kit_btns/script.js
@@ -26,24 +26,9 @@ function playAudio(parent) {
     audio.play();
 }
 
-function makeImgBigger(parent) {
-    const img = parent.querySelector('.buttonImg');
-    console.log(parent)
-    if (parent.id === 'kick-btn') {
-        img.style.backgroundSize = '130%';
-        setTimeout(() => img.style.backgroundSize = '120%', 100);
-    }
-    else {
-        img.style.backgroundSize = '110%';
-        setTimeout(() => img.style.backgroundSize = '100%', 100);
-    }
-}
-
 function makeBtnBigger(element) {
-    element.style.width = '270px';
-    element.style.height = '168px';
-    setTimeout(() => element.style.width = '250px', 100);
-    setTimeout(() => element.style.height = '150px', 100);
+    element.style.transform = 'scale(0.9)';
+    setTimeout(() => element.style.transform = 'scale(1)', 100);
 }
 
 main();
